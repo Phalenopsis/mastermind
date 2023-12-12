fcbddde89cac3264e2272b75e11a84db070f0ecf
Nicolas Leroux <hertford.fr@gmail.com>
2023-12-12 12:44:18 +0100

    end dojo

--- src/MasterMind.php
+++ src/MasterMind.php
@@ -4,8 +4,42 @@ namespace App;
 
 class MasterMind
 {
+    /**
+     * Fonction pour calculer le score au Mastermind
+     * Coucou Virginie
+     *
+     * @param array $masterColors -> les couleurs à chercher
+     * @param array $playerColors -> les couleurs proposées
+     * @return array|string
+     */
     public function getScore(array $masterColors, array $playerColors): array|string
     {
-        return '';
+        $goodPlaced = 0;
+        $badPlaced = 0;
+
+        // for ($i = 0; $i < count($masterColors); $i++) {
+        //     if (in_array($playerColors[$i], $masterColors)) {
+        //         if ($i === array_search($playerColors[$i], $masterColors)) {
+        //             $goodPlaced += 1;
+        //         } else {
+        //             $badPlaced += 1;
+        //         }
+        //     }
+        // }
+        foreach ($playerColors as $key => $color) {
+            if (in_array($color, $masterColors)) {
+                if ($key === array_search($color, $masterColors)) {
+                    $goodPlaced += 1;
+                } else {
+                    $badPlaced += 1;
+                }
+            }
+        }
+
+
+        if ($goodPlaced === 4) {
+            return 'win';
+        }
+        return [$badPlaced, $goodPlaced];
     }
 }
--- tests/MasterMindTest.php
+++ tests/MasterMindTest.php
@@ -1,6 +1,56 @@
 <?php
 
-class MasterMindTest
-{
+use PHPUnit\Framework\TestCase; // classe abstraite dont hérite notre classe de test
+use App\MasterMind; // classe testée
+use PHPUnit\Framework\Attributes\DataProvider; // permet d'utiliser l'attribut DataProvider
 
+class MasterMindTest extends TestCase
+{
+    /**
+     * Data provider => retourne les valeurs passées en arguments à la fonction de test
+     *
+     * @return array
+     */
+    public static function getScoreProvider(): array
+    {
+        $masterColors = ['blue', 'green', 'white', 'black'];
+        return [
+            [$masterColors, ['purple', 'orange', 'pink', 'yellow'], [0, 0]],
+            [$masterColors, ['red', 'blue', 'green', 'pink'], [2, 0]],
+            [$masterColors, ['blue', 'orange', 'pink', 'yellow'], [0, 1]],
+            [$masterColors, ['purple', 'orange', 'pink', 'yellow'], [0, 0]],
+            [$masterColors, ['red', 'blue', 'green', 'black'], [2, 1]],
+            [$masterColors, ['blue', 'green', 'white', 'black'], 'win'],
+        ];
+    }
+
+    /**
+     * fonction de test pour tester Mastermind->getScore() 
+     * 
+     * @param array $master -> les couleurs du master
+     * @param array $player -> les couleurs du player
+     * @param array|string $expected -> le résultat attentu de la méthode avec ces deux entrées.
+     * @return void
+     */
+    #[DataProvider('getScoreProvider')] // indique à la fonction de test qui va lui fournir les valeurs
+    public function testGetScore($master, $player, $expected)
+    {
+        $solution = new MasterMind();
+
+        $this->assertSame($expected, $solution->getScore($master, $player)); //on vérifie que $expected === le résultat de la méthode testée
+
+        // Ce que l'on aurait du faire sans le provider
+
+        // $master = ['blue', 'green', 'green', 'black'];
+        // $player = ['red', 'blue', 'green', 'pink'];
+        // $this->assertSame([0, 1], $solution->getScore($master, $player));
+
+        // $master = ['blue', 'green', 'white', 'black'];
+        // $player = ['red', 'blue', 'green', 'pink'];
+        // $this->assertSame([2, 0], $solution->getScore($master, $player));
+
+        // $master = ['blue', 'green', 'white', 'black'];
+        // $player = ['blue', 'orange', 'pink', 'yellow'];
+        // $this->assertSame([2, 0], $solution->getScore($master, $player));
+    }
 }
